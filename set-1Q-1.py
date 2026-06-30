39f9cd2804ccf9ffa98d42e8fee9634824e7c441
Aishakhan27 <aishakhan2730@gmail.com>
2026-06-30 11:10:38 +0530

    Update set-1Q-1.py

--- set-1Q-1.py
+++ set-1Q-1.py
@@ -1,9 +1,5 @@
-# items=[1,2,3,4,5,6]
-# # output=[1,2],[3,4],[5,6]
-# k=2
-# for i in range(0, len(items), k):
-#     print(items[i:i+k])
-name=["ball","bat","glove","glove","glove"]
-price=[2,3,1,2,1]
-weight=[2,5,1,1,1]
-print(name[0],price[0],weight[0])
+items=[1,2,3,4,5,6]
+# output=[1,2],[3,4],[5,6]
+k=2
+for i in range(0, len(items), k):
+    print(items[i:i+k])
